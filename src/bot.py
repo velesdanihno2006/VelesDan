764de638320c7a14a4535b1623a91ca17419fc67
Matsnew Maksim <b1narycot@gmail.com>
2025-05-28 05:22:15 +0300

    horoscope bot: add message handler

--- src/bot.py
+++ src/bot.py
@@ -1,20 +1,17 @@
 import os
-
 import telebot
+import requests
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-@bot.message_handler(commands=["start", "hello"])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
-
-
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=["horoscope"])
+def sign_handler(message):
+    text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, day_handler)
 
 
 bot.infinity_polling()
